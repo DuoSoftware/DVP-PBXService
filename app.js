399cd4aa2469ea36774c487e5bb84ae6104d074e
dinusha_dck <dinusha@duosoftware.com>
2015-11-13 10:48:36 +0530

    add ring timeout to output

--- app.js
+++ app.js
@@ -312,6 +312,7 @@ server.post('/DVP/API/:version/PBXService/GeneratePBXConfig', function(req, res,
                                                 pbxUserConf.Endpoints = tempEpList;
                                                 pbxUserConf.VoicemailEnabled = voicemailEnabled;
                                                 pbxUserConf.BypassMedia = bypassMedia;
+                                                pbxUserConf.RingTimeout = pbxDetails.RingTimeout;
 
                                                 if(pbxDetails.PersonalGreetingEnabled && pbxDetails.TimeZone)
                                                 {
