683227a61d64b9c890913a51a171cb479928433f
sukitha <sukitha@duosoftware.com>
2017-06-30 18:29:21 +0530

    ioredis added with sentinel support

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -32,20 +32,35 @@ module.exports = {
 
     "Redis":
     {
-        "IpAddress": "SYS_REDIS_HOST",
-        "Port": "SYS_REDIS_PORT",
-        "Password": "SYS_REDIS_PASSWORD",
-        "db": "SYS_REDIS_DB_CONFIG"
+        "mode":"SYS_REDIS_MODE",
+        "ip": "SYS_REDIS_HOST",
+        "port": "SYS_REDIS_PORT",
+        "user": "SYS_REDIS_USER",
+        "password": "SYS_REDIS_PASSWORD",
+        "sentinels":{
+            "hosts": "SYS_REDIS_SENTINEL_HOSTS",
+            "port":"SYS_REDIS_SENTINEL_PORT",
+            "name":"SYS_REDIS_SENTINEL_NAME"
+        }
+
     },
 
     "Security":
     {
+
         "ip": "SYS_REDIS_HOST",
         "port": "SYS_REDIS_PORT",
         "user": "SYS_REDIS_USER",
-        "password": "SYS_REDIS_PASSWORD"
+        "password": "SYS_REDIS_PASSWORD",
+        "mode":"SYS_REDIS_MODE",
+        "sentinels":{
+            "hosts": "SYS_REDIS_SENTINEL_HOSTS",
+            "port":"SYS_REDIS_SENTINEL_PORT",
+            "name":"SYS_REDIS_SENTINEL_NAME"
+        }
 
     },
+
 
     "Token": "HOST_TOKEN"
 };
